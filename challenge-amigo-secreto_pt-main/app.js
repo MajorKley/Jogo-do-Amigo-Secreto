dc0efc09c8531d06f5312bbed121e83c7710a431
Kleyciane D. <kleyciane.d.mendes@gmail.com>
2025-08-08 21:02:48 -0300

    Arquivos alterados e projeto iniciado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const lista = [];
+
+
+function adicionarAmigo(){
+    let lista_amigos = document.getElementById('amigo').value
+    lista.push(lista_amigos);
+    console.log(lista)
+}
