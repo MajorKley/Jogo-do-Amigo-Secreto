740ff9c75a8c5594bc7ce0e512daa6c5c9abc557
Kleyciane D. <kleyciane.d.mendes@gmail.com>
2025-08-13 20:38:57 -0300

    Adiconado funções para o sorteio

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,9 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 const lista = [];
 
+function adicionarAmigo() {
 
-function adicionarAmigo(){
-    let lista_amigos = document.getElementById('amigo').value
-    lista.push(lista_amigos);
-    console.log(lista)
+    let inputLista = document.getElementById('amigo').value
+
+    if (inputLista == " ") {
+        alert("Por favor, insira um nome")
+        return false
+    }
+
+    lista.push(inputLista);
+    document.getElementById('listaAmigos').innerHTML = lista
+    document.getElementById('amigo').value = " "
+   
+}
+
+
+function processo_sorteio(amigos){
+    const sorteio = Math.floor(Math.random() * amigos.length)
+    return amigos[sorteio]
+    
+}
+
+function sortearAmigo(){
+
+    const resultadosorteio = processo_sorteio(lista)
+    document.getElementById("resultado").innerHTML = resultadosorteio
+
 }
